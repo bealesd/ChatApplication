3e98eb348979d9c9100a8cde3b5056e853c1a931
David Beales <33747318+bealesd@users.noreply.github.com>
2020-08-14 14:27:55 +0100

    Update chatHelper.js
    Quick fix due to extra attribute being added to a chat object.

--- ChatApplication/wwwroot/scripts/chatHelper.js
+++ ChatApplication/wwwroot/scripts/chatHelper.js
@@ -24,7 +24,7 @@ export class ChatHelper {
 
     static createMessageNode(messageObject) {
         const messagesProperties = ['Id', 'Who', 'Datetime', 'Content'];
-        if (!CoreHelper.hasProperiesOfStringOrNumberOnly(messageObject, messagesProperties)) return null;
+        //if (!CoreHelper.hasProperiesOfStringOrNumberOnly(messageObject, messagesProperties)) return null;
 
         const messageDateTime = new Date(messageObject['Datetime']);
         return `<div data-id=${messageObject['Id']} class="${messageObject['Who'].toLowerCase()} messageNode">
